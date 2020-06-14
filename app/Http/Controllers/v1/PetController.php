3399d096da4b0be436e81b581632e56ee6006244
Lucas Ennes <lucassilvaennes@gmail.com>
2020-06-14 14:49:29 -0300

    endpoints para Pet

---
+++ app/Http/Controllers/v1/PetController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\v1;
+
+use App\Pet;
+use App\Http\Resources\v1\PetResource;
+use App\Http\Resources\v1\PetResourceCollection;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PetController extends Controller
+{
+
+    /**
+     * @return PetResourceCollection
+     */
+    public function index(): PetResourceCollection
+    {
+        return new PetResourceCollection(Pet::paginate());
+    }
+
+    /**
+     * @param Pet $pet
+     * @return PetResource
+     */
+    public function show(Pet $pet): PetResource
+    {
+        return new PetResource($pet);
+    }
+
+    /**
+     * @param Request $request
+     * @return PetResource
+     */
+    public function store(Request $request): PetResource
+    {
+        $request->validate([
+            'nome' => 'required',
+            'especie' => 'required',
+        ]);
+        $pet = Pet::create($request->all());
+        return new PetResource($pet);
+    }
+
+    /**
+     * @param Request $request
+     * @return PetResource
+     */
+    public function update(Pet $pet, Request $request): PetResource
+    {
+        $pet->update($request->all());
+
+        return new PetResource($pet);
+    }
+
+    /**
+     * @param Pet $pet
+     * @return \Illuminate\Http\JsonResponse
+     * @throws \Exception
+     */
+    public function destroy(Pet $pet)
+    {
+        $pet->delete();
+        return response()->json();
+    }
+}
